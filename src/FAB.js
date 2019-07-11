62a26baa131c58b308be8a158e5acaf2813bfa3b
KunJ99 <kunjeong99@naver.com>
2019-07-11 23:27:05 +0900

    Code minimized

--- src/FAB.js
+++ src/FAB.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableWithoutFeedback, Animated, Text, View, StyleSheet, PanResponder, Dimensions} from 'react-native'
+import { TouchableWithoutFeedback, Animated, Text, View, StyleSheet, PanResponder, Dimensions } from 'react-native'
 import { Icon } from 'react-native-elements';
 
 export default class FAB extends TouchableWithoutFeedback{
@@ -9,84 +9,52 @@ export default class FAB extends TouchableWithoutFeedback{
           scale : new Animated.Value(1),
           pan: new Animated.ValueXY()
         }
+    }
+    componentWillMount() {
         this.panResponder = PanResponder.create({
-          // onStartShouldSetPanResponder : () => true,
-          onMoveShouldSetResponderCapture: () => true,
-          onMoveShouldSetPanResponderCapture: () => true,
-
-          onPanResponderGrant: (evt, gestureState) => {
-            Animated.spring(
-              this.state.scale,
-              {
-                toValue: 1.3,
-                bounciness: 15,
-                speed: 40
-              }
-            ).start()
-          },
-          onPanResponderMove: Animated.event([null, {
-            dx: this.state.pan.x,
-            dy: this.state.pan.y
-          }]),
-          onPanResponderRelease: (e, gesture) => {
-            Animated.parallel([
-              Animated.spring(
-                this.state.pan,
-                {
-                  toValue:{x:0,y:0},
-                  bounciness: 8,
-                  speed: 20
-                }
-              ).start(),
-              Animated.spring(
+            onMoveShouldSetResponderCapture: () => true,
+            onMoveShouldSetPanResponderCapture: () => true,
+ 
+            onPanResponderGrant: (evt, gestureState) => {
+                Animated.spring(
                 this.state.scale,
                 {
-                  toValue: 1,
-                  bounciness: 15,
-                  speed: 40
+                    toValue: 1.3,
+                    bounciness: 15,
+                    speed: 40,
+                    useNativeDriver: true
                 }
-              ).start()
-            ])
-              
-          }
+                ).start()
+            },
+            onPanResponderMove: Animated.event([null, {
+                dx: this.state.pan.x,
+                dy: this.state.pan.y
+            }]),
+            onPanResponderRelease: (e, gesture) => {
+                Animated.parallel([
+                    Animated.spring(
+                        this.state.pan,
+                        {
+                        toValue:{x:0,y:0},
+                        bounciness: 8,
+                        speed: 20,
+                        useNativeDriver: true
+                        }
+                    ).start(),
+                    Animated.spring(
+                        this.state.scale,
+                        {
+                        toValue: 1,
+                        bounciness: 15,
+                        speed: 40,
+                        useNativeDriver: true
+                        }
+                    ).start()
+                ])
+            }
         })
     }
-    style = {
-      position: 'absolute',
-        
-      //bottom: Animated.subtract(50, Animated.divide(this.state.size, 2)),
-      // right: Animated.subtract(50, Animated.divide(this.state.size, 2)),
-      width: 60,
-      height: 60,
-      // width: this.state.size,
-      // height: this.state.size,
-      //transform: [{translateX: 0}, {this.state.scale}],
-      borderRadius: 30,
-      //borderRadius: Animated.divide(this.state.size, 2),
-      backgroundColor: '#667788',
-      justifyContent: 'center',
-      
-    }
-    onPressIn = () => {
-        Animated.spring(
-            this.state.size,
-            {
-              toValue: 80,
-              bounciness: 15,
-              speed: 40
-            }
-        ).start()
-    }
-    onPressOut = () => {
-        Animated.spring(
-            this.state.size,
-            {
-              toValue: 60,
-              bounciness: 15,
-              speed: 40
-            }
-        ).start()
-    }
+
     render(){
       let { pan, scale } = this.state;
       let [translateX, translateY] = [pan.x, pan.y];
@@ -101,12 +69,7 @@ export default class FAB extends TouchableWithoutFeedback{
       };
       
         return(    
-          <View style={styles.container}>
             <View style={styles.absolute}>
-              {/* <TouchableWithoutFeedback
-              onPressIn={this.onPressIn.bind(this)}
-              onPressOut={this.onPressOut.bind(this)}
-              > */}
                 <Animated.View
                 {...this.panResponder.panHandlers} 
                 style={imageStyle}>
@@ -116,24 +79,17 @@ export default class FAB extends TouchableWithoutFeedback{
                   size = {40}
                   />
                 </Animated.View>
-              {/* </TouchableWithoutFeedback> */}
             </View>
-          </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
   absolute: {
-    position: 'absolute', backgroundColor: '#FFFFFF', right: 20, bottom: 20, height: 60, width: 60  
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    height: 60,
+    width: 60  
   }
 });
-
-//export default FAB;
